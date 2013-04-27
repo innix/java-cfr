4d6194d67f93d7e8539b07b89bd099fe3958fd86
Phil Keeling <philip.keeling@outlook.com>
2013-04-27 22:58:24 +0100

    Correctly implemented decoding UTF8 const pool entries. Other small changes.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 var jcfr = require("./lib/jcfr");
 
-var json = jcfr.readFile("C:\\Users\\Philip\\IdeaProjects\\untitled\\out\\production\\untitled\\App.class");
+var json = jcfr.readFile("C:\\Users\\Philip\\IdeaProjects\\untitled\\out\\production\\untitled\\com\\helborn\\Foo.class");
 console.log(json);
